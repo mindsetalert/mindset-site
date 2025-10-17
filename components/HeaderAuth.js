ccfc8d23050642f6d48108e9f030b0c85fdd2e0e
Celine <mindsetalertstrategy@hotmail.com>
2025-10-17 14:38:42 -0400

    Ajout bouton Accueil dans le header

--- components/HeaderAuth.js
+++ components/HeaderAuth.js
@@ -36,7 +36,7 @@ export default function HeaderAuth() {
   return (
     <div className="border-b border-neutral-800 bg-neutral-950">
       <div className="mx-auto max-w-5xl px-4 py-3 flex items-center justify-between">
-        <div />
+        <Link href="/" className="text-amber-400 hover:underline">{t('header.home')}</Link>
         {loading ? (
           <div className="text-neutral-400 text-sm">{t('header.loading')}</div>
         ) : user ? (
