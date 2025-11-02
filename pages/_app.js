4baa058d9deb0c9137979c111660fb461875e80e
Celine <mindsetalertstrategy@hotmail.com>
2025-11-02 14:05:38 -0500

    Restaure la barre 'Accueil / Mon compte / Connexion' (HeaderAuth) sous le header principal

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import { LanguageProvider } from "../contexts/LanguageContext";
 import SiteHeader from "../components/SiteHeader";
+import HeaderAuth from "../components/HeaderAuth";
 
 export default function App({ Component, pageProps }) {
   return (
     <LanguageProvider>
       <SiteHeader />
+      <HeaderAuth />
       <Component {...pageProps} />
     </LanguageProvider>
   );
